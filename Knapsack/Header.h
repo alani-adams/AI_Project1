72a75628e768b20e1ce16067aff4a9a5ee1b187f
cxs13b <cxs13b@acu.edu>
2017-01-29 14:44:35 -0600

    Add files via upload

---
+++ Knapsack/Header.h
@@ -0,0 +1,37 @@
+//
+//  Header.h
+//  AI_Project1
+//
+//  Created by Alani Peters on 1/19/17.
+//  Copyright © 2017 Alani Peters. All rights reserved.
+//
+
+
+class Item {
+    
+    private int value;
+    private int cost; //weight
+    private string name; //one letter char
+    
+    public int getValue() {return value;}
+    public int getCost() {return cost;}
+    
+};
+
+class Knapsack {
+    
+    private int limit;
+    private float currentValue;
+    
+    private int highestValueTotal;
+    private int lowestCostTotal;
+    private int valueCostRatioTotal;
+    private int partialTotal;
+    
+    
+    public int getLimit() {return limit};
+    public void setHighestValueTotal (int a) {highestValueTotal = a);
+    public void setLowestCostTotal (int a) {lowestCostTotal = a);
+    public void setValueCostRatioTotal (int a) {valueCostRatioTotal = a);
+    public void setPartialTotal (int a) {partialTotal = a);
+};
