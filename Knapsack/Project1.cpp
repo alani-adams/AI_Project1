f1a38c77fbc1d7f0f8af6021216fafb35190fcc0
alp13d <alp13d@acu.edu>
2017-01-30 13:49:12 -0600

    Add files via upload

---
+++ Knapsack/Project1.cpp
@@ -0,0 +1,33 @@
+//
+//  Project1.cpp
+//  
+//
+//  Created by Alani Peters on 1/30/17.
+//
+//
+
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include "Header.h"
+
+float Knapsack::highestValueFunction(vector <Item> items)
+{
+    
+    sortHighestValue(????);
+}
+
+float Knapsack::lowestCostFunction(vector <Item> items)
+{
+    sortLowestCost(????);
+}
+
+float Knapsack::valueCostRatioFunction(vector <Item> items)
+{
+    sortValueCostRatio(????);
+}
+
+float Knapsack::partialTotalFunction(vector <Item> items)
+{
+    sortPartialTotal(????);
+}
