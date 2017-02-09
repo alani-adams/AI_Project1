aa708fa9072379a46dcd2ecaf16a1ea5dc438666
alp13d <alp13d@acu.edu>
2017-02-09 16:02:28 -0600

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,97 @@
+//
+//  Header.h
+//  AI_Project1
+//
+//  Created by Alani Peters on 1/19/17.
+//  Copyright © 2017 Alani Peters. All rights reserved.
+//
+
+
+class Item {
+
+private:
+    float value;
+    float cost; //weight
+    string name; //one letter char
+    
+public:
+    float getValue() {return value;}
+    float getCost() {return cost;}
+    string getName() {return name;}
+    void setValue(int a) {value = a;}
+    void setCost (int a) {cost = a;}
+    void setName (string a) {name = a;}
+        
+};
+    
+class Knapsack {
+    
+private:
+    float limit;
+    float currentValue;
+    
+    float highestValueTotal;
+    float lowestCostTotal;
+    float valueCostRatioTotal;
+    float partialTotal;
+    
+public:
+    float getLimit() {return limit;}
+    void setLimit(int a) {limit = a;}
+    void setHighestValueTotal (int a) {highestValueTotal = a;}
+    void setLowestCostTotal (int a) {lowestCostTotal = a;}
+    void setValueCostRatioTotal (int a) {valueCostRatioTotal = a;}
+    void setPartialTotal (int a) {partialTotal = a;}
+
+    //http://www.cplusplus.com/reference/algorithm/sort/
+    static bool sortHighestValue(Item i, Item j) { return (i.getValue() > j.getValue());}
+    static bool sortLowestCost(Item i, Item j) { return (i.getCost() < j.getCost());}
+    static bool sortValueCostRatio(Item i, Item j) { return (((float)(i.getValue() / i.getCost()) > (float)(j.getValue() / j.getCost())));}
+    static bool sortPartialTotal(Item i, Item j) { return (((float)(i.getValue() / i.getCost()) > (float)(j.getValue() / j.getCost())));}
+
+    static float calculateGreedySolution(vector<Item> v, Knapsack k){
+
+        float sol = 0;
+        float i = 0;
+        float currentWeight = 0;
+
+        for ( i = 0; i < v.size(); i++)
+        {
+            if ( currentWeight + v[i].getCost() <= k.getLimit()) //if we can take an item
+            {
+                sol += v[i].getValue();
+                currentWeight += v[i].getCost();
+            }
+            else 
+                break; //if there is ever a case where our knapsack will be too large, we break, taking greedy solution
+        }   
+
+        return sol;
+    }
+
+    static float calculatePartialKnapsackSolution(vector<Item> v, Knapsack k){
+
+        float sol = 0; //solution
+        float i = 0; //counter
+        float currentWeight = 0;
+
+        for ( i = 0; i < v.size(); i++)
+        {
+            if ( currentWeight + v[i].getCost() <= k.getLimit()) //if we can take an item
+            {
+                sol += v[i].getValue();
+                currentWeight += v[i].getCost();
+            }
+            else
+            {
+                float difference = k.getLimit() - currentWeight;
+                sol = sol + (v[i].getValue() * (v[i].getCost()/difference));
+                //Get the partial of the next item, up to limit, then break
+                break;
+            }
+        }   
+
+        return sol;
+    }
+    
+};
---
+++ main.cpp
@@ -0,0 +1,143 @@
+//
+//  main.cpp
+//  AI_Project1
+//
+//  Created by Alani Peters on 1/19/17.
+//  Copyright © 2017 Alani Peters. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cstdio>
+#include <ctime>
+
+
+#include "Header.h"
+#include "bst.h"
+
+using namespace std;
+
+void printItemVector(vector<Item> items)
+{
+    for (int i=0; i < items.size(); i++){
+        cout << "Name: " << items[i].getName() << " Cost:" << items[i].getCost() << " Value: " << items[i].getValue() << endl;
+    }
+}
+
+Tree exhaustiveSearch(vector<Item> items)
+{
+    Tree bst;
+    bst.initialize();
+    for(int i=0; i<items.size(); i++)
+    {
+        insert(items[i]);
+    }
+
+
+    return bst;
+}
+
+int main() 
+{
+    
+    Knapsack knapsack;
+    vector <Item> items;
+    string filename;
+    cout <<"Insert file name: ";
+    cin >> filename;
+    Item temp;
+    fstream fs;
+    fs.open (filename);
+    if (fs.is_open())
+    {
+        string line1;
+        int maxWeight;
+        getline(fs, line1);
+        stringstream ss(line1);
+        ss >> maxWeight;
+        
+        knapsack.setLimit(maxWeight);
+        
+        while(getline(fs, line1))//get all the items
+        {
+            
+            std::istringstream ss(line1);
+            std::string token;
+            int count=0;
+            string name;
+            string cost;
+            string value;
+            string i;
+            while(getline(ss, token, ','))
+            {
+                
+                if(count==0)
+                    name=token;
+                else if(count==1)
+                    cost=token;
+                else
+                    value=token;
+                
+                count++;
+            }
+            
+            //cout<<"name "<<name<<endl;
+            temp.setName(name);
+            int cost1 = atoi(cost.c_str());
+            //cout<<"cost "<<cost1<<endl;
+            temp.setCost(cost1);
+            int value1 = atoi(value.c_str());
+            //cout<<"value "<<value1<<endl;
+            temp.setValue(value1);
+            items.push_back(temp);
+        }
+    }
+    fs.close();
+
+    cout << endl << "Limit: " << knapsack.getLimit() << endl << endl;
+
+    sort(items.begin(), items.end(), Knapsack::sortHighestValue);
+    cout<<"Highest Value First: "<< endl;
+    //printItemVector(items);    
+    cout << "Solution: "<< Knapsack::calculateGreedySolution(items, knapsack) << endl << endl;
+
+    sort(items.begin(), items.end(), Knapsack::sortLowestCost);
+    cout<<"Lowest Cost First: "<<endl;
+    //printItemVector(items);
+    cout << "Solution: "<< Knapsack::calculateGreedySolution(items, knapsack) << endl << endl;
+
+    sort(items.begin(), items.end(), Knapsack::sortValueCostRatio);
+    cout<<"Highest Ratio First: "<<endl;
+    //printItemVector(items);
+    cout << "Solution: "<< Knapsack::calculateGreedySolution(items, knapsack) << endl << endl;
+    
+    sort(items.begin(), items.end(), Knapsack::sortPartialTotal);
+    cout<<"Partial Knapsack: "<<endl;
+    cout << "Solution: "<< Knapsack::calculatePartialKnapsackSolution(items, knapsack) << endl << endl;
+    //printItemVector(items);
+    
+    //exhaustive search
+
+    Tree bst = exhaustiveSearch(items);
+    
+
+
+
+    bst.destroy();
+ /* HOW TO DO A TIMER 
+    std::clock_t start;
+    double duration;
+
+    start = std::clock();
+
+    //Your algorithm here
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+
+    std::cout<<"printf: "<< duration <<'\n';
+ */
+}
